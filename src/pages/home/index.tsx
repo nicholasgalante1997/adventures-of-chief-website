151add2bc7e8c01e0276c155eb5823a73b86d95c
Nick G <nicholasgalante1997@gmail.com>
2022-05-31 22:59:13 -0600

    initial

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Typography, Button } from '@nickgdev/hellerui';
+
+const { Heading } = Typography;
+
+export function Home() {
+  return (
+    <Container>
+      <Heading>chief!</Heading>
+    </Container>
+  );
+}
